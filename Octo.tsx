d84d2112900d9629580e82b11460cfffe66d6a02
djamelbn <36231936+djamelbn@users.noreply.github.com>
2021-02-24 17:29:38 +0100

    Add files via upload

---
+++ Octo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="octo" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="octo-logo.png" trans="ff00ff" width="128" height="49"/>
+</tileset>
